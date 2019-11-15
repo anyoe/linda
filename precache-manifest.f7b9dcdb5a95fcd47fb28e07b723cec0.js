298f3b2804b3dd5c1a7f9c2c1ffc9d325cf43e42
bigmodem <bigmodem@gmail.com>
2019-11-15 19:13:28 +0800

    Updates

---
+++ precache-manifest.f7b9dcdb5a95fcd47fb28e07b723cec0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fe40b5df421e9cdc1c180283cf5ad6",
+    "url": "/linda/index.html"
+  },
+  {
+    "revision": "0224e2d556c7bb4c3346",
+    "url": "/linda/static/js/2.374a4026.chunk.js"
+  },
+  {
+    "revision": "f733107d85bf8d4a1ff3",
+    "url": "/linda/static/js/main.c7c00b5e.chunk.js"
+  },
+  {
+    "revision": "70eb218f453868f9ea3f",
+    "url": "/linda/static/js/runtime-main.d6c9394a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/linda/static/media/logo.25bf045c.svg"
+  }
+]);
